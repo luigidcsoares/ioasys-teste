4d4340e7477bcdb4e56bdbd8f64baa7173257ed9
Luigi D. C. Soares <luigi.soares@sga.pucminas.br>
2019-02-01 18:22:09 -0200

    main activity & login

---
+++ app/src/main/java/br/com/ioasys/teste/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.ioasys.teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ioasys.teste.ui.login.LoginFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LoginFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/br/com/ioasys/teste/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.ioasys.teste.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
